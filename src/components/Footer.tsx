8db7b9157e6c42d1870d4045f9f88fcdd1a33497
Kangdims <49813679+SamiedKode@users.noreply.github.com>
2026-02-25 22:35:46 +0700

    Update footer links and copyright information

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,33 +20,33 @@ export function Footer() {
           {/* <ul className="space-y-2 text-sm">
               <li>
                 <a 
-                  href="https://lynk.id/sansekai/6gdx3w875wx1" 
+                  href="https://lynk.id/kangdims" 
                   target="_blank" 
                   rel="noopener noreferrer"
                   className="text-muted-foreground hover:text-primary transition-colors inline-flex items-center gap-1 text-primary hover:underline font-semibold"
                 >
-                  Beli Source Code Website Ini
+                  Nonton Gratis Full Episodea
                   <ExternalLink className="w-3 h-3" />
                 </a>
               </li>
           </ul> */}
 
           <p className="text-sm text-muted-foreground text-center">
-            API yang digunakan:{" "}
+            Gratis Tanpa :{" "}
             <a 
-                  href="https://api.sansekai.my.id" 
+                  href="https://kangdims.com" 
                   target="_blank" 
                   rel="noopener noreferrer"
                   className="text-muted-foreground hover:text-primary transition-colors inline-flex items-center gap-1 text-primary hover:underline font-semibold"
                 >
-                  SΛNSΞKΛI API
+                  kangdrama
                   <ExternalLink className="w-3 h-3" />
                 </a>
           </p>
 
           {/* Copyright */}
           <p className="text-xs text-muted-foreground/80 text-center font-medium">
-            © {new Date().getFullYear()} Made with ❤️ by Yusril
+            © {new Date().getFullYear()} Made with ❤️ by kangdims
           </p>
         </div>
       </div>
